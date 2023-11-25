2e06e8f4c53b06fa15f6d1b48379e9b290584cd1
yash-matchmyflight <yash.varshney@matchmyflight.com>
2023-11-25 16:23:35 +0530

    change the routing

--- src/components/Event/Events.js
+++ src/components/Event/Events.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import "./Events.css";
 
 const Events = () => {
+  const [events, setEvents] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -13,7 +16,8 @@ const Events = () => {
           },
         });
         const data = await response.json();
-        console.log(data);
+        console.log(data?.getEvent);
+        setEvents(data?.getEvent);
       } catch (error) {
         console.error(error);
       }
@@ -22,7 +26,28 @@ const Events = () => {
     fetchData();
   }, []);
 
-  return <div>Events</div>;
+  return (
+    <div>
+      {events?.map((event) => (
+        <div key={event?.event_name} className="card">
+          <img
+            src="https://images.unsplash.com/photo-1621758149070-5a9d6a4d8f7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y29sbGVjdGlvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{event?.event_name}</h5>
+            <p className="card-text">
+              Start Date: {new Date(event?.start_date).toLocaleDateString()}
+            </p>
+            <p className="card-text">
+              End Date: {new Date(event?.end_date).toLocaleDateString()}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Events;
